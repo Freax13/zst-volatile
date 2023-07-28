e3469b6f8bb43c7589a51100a8a0955b02f3cab5
Tom Dohrmann <erbse.13@gmx.de>
2023-07-28 20:55:25 +0200

    ensure that the struct is #[repr(C)]

--- zst-volatile-macro/src/lib.rs
+++ zst-volatile-macro/src/lib.rs
@@ -1,16 +1,27 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Error};
+use syn::{parse_macro_input, Attribute, Data, DeriveInput, Error, Lit, Meta, NestedMeta, Result};
 
 #[proc_macro_derive(VolatileStruct)]
 pub fn derive_volatile(tts: TokenStream) -> TokenStream {
-    // FIXME: Check for repr(C), currently we just assume this.
-
     let input = parse_macro_input!(tts as DeriveInput);
 
     let Data::Struct(strukt) = &input.data else {
-        return Error::new_spanned(&input,"only structs are supported").into_compile_error().into();
+        return Error::new_spanned(&input, "only structs are supported")
+            .into_compile_error()
+            .into();
     };
+
+    let packed = match check_repr(&input.attrs) {
+        Ok(packed) => packed,
+        Err(err) => return err.into_compile_error().into(),
+    };
+    if packed.is_some() {
+        return Error::new(Span::call_site(), "packed structs are not supported")
+            .into_compile_error()
+            .into();
+    }
 
     let vis = &input.vis;
     let ident = &input.ident;
@@ -48,4 +59,55 @@ pub fn derive_volatile(tts: TokenStream) -> TokenStream {
         }
     }
     .into()
+}
+
+fn check_repr(attrs: &[Attribute]) -> Result<Option<usize>> {
+    let mut is_c_repr = false;
+    let mut packed = None;
+
+    for attr in attrs.iter().filter(|a| a.path.is_ident("repr")) {
+        let meta = attr.parse_meta()?;
+        let Meta::List(list) = meta else {
+            return Err(Error::new_spanned(meta, "expected list"));
+        };
+        for nested_meta in list.nested {
+            let NestedMeta::Meta(m) = nested_meta else {
+                return Err(Error::new_spanned(nested_meta, "expected meta"));
+            };
+            if m.path().is_ident("C") {
+                is_c_repr = true;
+            } else if m.path().is_ident("packed") {
+                match m {
+                    Meta::Path(_) => packed = Some(1),
+                    Meta::List(list) => {
+                        if list.nested.len() != 1 {
+                            return Err(Error::new_spanned(list, "expected exactly one value"));
+                        }
+                        let first = list.nested.first().unwrap();
+                        let NestedMeta::Lit(lit) = first else {
+                            return Err(Error::new_spanned(list, "expected literal"));
+                        };
+                        let Lit::Int(lit_int) = lit else {
+                            return Err(Error::new_spanned(list, "expected integer literal"));
+                        };
+                        let value = lit_int.base10_parse()?;
+                        packed = Some(value);
+                    }
+                    Meta::NameValue(_) => {
+                        return Err(Error::new_spanned(m, "expected path or list meta"));
+                    }
+                }
+            } else if m.path().is_ident("align") {
+                // Ignore
+            } else {
+                return Err(Error::new_spanned(m, "unexpected attribute"));
+            }
+        }
+    }
+
+    if !is_c_repr {
+        return Err(Error::new(Span::call_site(), "struct has to be #[repr(C)]"));
+    }
+
+    Ok(packed)
 }
